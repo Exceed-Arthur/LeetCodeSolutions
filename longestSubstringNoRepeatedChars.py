793219d9820c677398a06449418bb65c8363f3ac
Exceed IO Inc <arthurlee087@gmail.com>
2022-09-06 02:22:51 -0500

    Update longestSubstringNoRepeatedChars.py

--- longestSubstringNoRepeatedChars.py
+++ longestSubstringNoRepeatedChars.py
@@ -1,6 +1,8 @@
 class Solution:
+
     def lengthOfLongestSubstring(self, s: str, largest=0) -> int:
         largest = largest
+        print(f"Length Of String We Are Analyzing {s}: {len(s)}")
         if len(s) == 0:
             return 0
         if len(set(s)) == 1:
@@ -16,8 +18,10 @@ class Solution:
                     if let in usedLetters:
                         if not largest > index1:
                             largest = index1
+                            print(f"changed largest to {index1}")
                         if largest >= len(s[index1:len(s)]):
                             return largest
                         return Solution.lengthOfLongestSubstring(self, s[index1:len(s)], largest)
                     usedLetters.append(let)
                     index1+=1
+                print(remainingChars, "remaining chars", s[index:len(s)])
